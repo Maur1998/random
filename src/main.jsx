61a1aaa23e3011ae4dadbfbea62c1f9c93dd203c
Maur1998 <mmurillosanchez1998@gmail.com>
2023-07-05 11:42:40 -0400

    creando random

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Prueba1} from './components/Prueba1'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Prueba1 />
+  </React.StrictMode>,
+)
